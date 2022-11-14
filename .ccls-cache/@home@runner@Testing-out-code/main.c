4fb0e3b331032247a251d647719c3493cac9154e
mambriz <>
2022-11-14 23:30:18 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Testing-out-code/main.c
@@ -0,0 +1,15 @@
+#include<string.h>
+#include <stdio.h>   // For printf and scanf
+
+
+
+int main () {
+   int n;
+
+   printf("Before sorting the list is: \n");
+  
+   printf("\nAfter sorting the list is: \n");
+
+  
+   return(0);
+}
